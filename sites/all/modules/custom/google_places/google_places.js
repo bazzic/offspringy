6d343e5979e8a37ff78875404f5deaa8e02829c6
Nate Garbacz <bazzic@gmail.com>
2015-05-19 10:40:17 +0100

    Code for populating the accommodation form.

--- sites/all/modules/custom/google_places/google_places.js
+++ sites/all/modules/custom/google_places/google_places.js
@@ -16,7 +16,45 @@
 	        
 	        $("#edit-geocomplete").geocomplete(options)
 	          .bind("geocode:result", function(event, result){
+	        	  var address1 = '';
+	        	  var city = '';
+	        	  var country = '';
+	        	  var postal_code = '';
+	        	  alert(JSON.stringify(result.formatted_phone_number));
+	        	  alert(JSON.stringify(result.website));
+	        	  result.forEach(function (item) {
+	        		  alert(JSON.stringify(item));
+	        	  });
+	        	  
+	        	  result.address_components.forEach(function (item) {
+	        		  alert(JSON.stringify(item));
+	        		  if (item['types'] == 'street_number') {
+	        			  address1 += item['long_name'] + " ";
+	        			  alert(JSON.stringify(item));
+	        		  } else if (item['types'] == 'route') {
+	        			  address1 += item['long_name'];
+	        		  } else if (item['types'] == 'postal_town') {
+	        			  city += item['long_name'];
+	        		  } else if ($.inArray('locality', item['types']) > -1) {
+	        			  city += item['long_name'];
+	        		  } else if ($.inArray('country', item['types']) > -1) {
+	        			  country += item['short_name'].toLowerCase();
+	        		  } else if (item['types'] == 'postal_code') {
+	        			  postal_code += item['long_name'];
+	        		  }
+	        	  });
+	        	  alert(country);
+
+	        	  $("#edit-title").val(result.name);
+	        	  $("#edit-locations-0-name").val(result.name);
+	        	  $("#edit-locations-0-street").val(address1);
+	        	  $("#edit-locations-0-city").val(city);
+	        	  $("#edit-locations-0-postal-code").val(postal_code);
+	        	  $("#edit-locations-0-country").val(country);
+	        	  $("#edit-locations-0-locpick-user-latitude").val(result.geometry.location.lat());
+	        	  $("#edit-locations-0-locpick-user-longitude").val(result.geometry.location.lng());
 	            $.log("Address: " + result.formatted_address + " Lat: " + result.geometry.location.lat() + " Lon: " + result.geometry.location.lng());
+	            
 	          })
 	          .bind("geocode:error", function(event, status){
 	            $.log("ERROR: " + status);
